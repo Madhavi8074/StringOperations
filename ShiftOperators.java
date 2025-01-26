4c787382b1bc192017552554afcbf6f69bf74233
Madhavi8074 <madhavi738685@gmail.com>
2025-01-26 18:58:36 +0530

    Add files via upload

---
+++ ShiftOperators.java
@@ -0,0 +1,10 @@
+public class ShiftOperators {
+    public static void main(String[] args){
+        int num1=5;
+        int leftShift=num1<<1;
+        System.out.println("Left Shift (5<<1): "+leftShift);
+        int num2=5;
+        int rightShift=num2>>1;
+        System.out.println("Right Shift (20>>2): "+rightShift);
+    }
+}
